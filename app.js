b18d9e7a641e3f18d82aa819f3d5622a48be241b
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-07-28 13:15:19 -0400

    edit page with coresp PUT request

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 var app = express();
 
 mongoose.connect('mongodb://localhost/tk-blog');
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -17,11 +19,11 @@ var blogSchema = new mongoose.Schema({
 var Blog = mongoose.model("Blog", blogSchema);
 
 app.get('/posts', function(req, res){
-    Blog.find({}, function(err, blogs){
+    Blog.find({}, function(err, blog){
         if(err){
             console.log("ERROR!");
         } else {
-            res.render("index", {blogs: blogs});
+            res.render("index", {blog: blog});
         }
     })
 });
@@ -33,7 +35,28 @@ app.get('/', function(req, res){
 
 app.get('/posts/new', function(req, res){
     res.render('new');
-})
+});
+
+app.get('/posts/:id', function(req, res){
+    Blog.findById(req.params.id, function(err, foundPost){
+        if(err){
+            res.redirect('/posts');
+        } else {
+            res.render('show', {post: foundPost});
+        }
+    });
+});
+
+app.get('/posts/:id/edit', function(req, res){
+    Blog.findById(req.params.id, function(err, foundPost){
+        if (err) {
+            res.redirect("/posts")
+        } else {
+            res.render('edit', {post: foundPost})
+        }
+    });
+});
+
 
 app.post("/posts", function(req, res){
     Blog.create(req.body.blog, function(err, newBlog){
@@ -44,6 +67,16 @@ app.post("/posts", function(req, res){
         }
     })
 })
+
+app.put('/posts/:id', function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog) {
+        if (err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
 
 app.listen(process.env.PORT || 4000, function(){
     console.log('SERVER IS RUNNING');
